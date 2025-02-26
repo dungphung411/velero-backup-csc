f31159184890b54ae56276f14addd52ef0c37055
dungphung411 <mdungphung411@gmail.com>
2025-02-26 09:58:40 +0700

    Update deploy.sh

--- Wordpress_deploy_backup/deploy.sh
+++ Wordpress_deploy_backup/deploy.sh
@@ -1,12 +1,14 @@
 #!bin/bash
+# Apply Storage_PV_PVC
 kubectl apply -f storageclass_cloudian.yaml
 kubectl apply -f namespace.yaml
 kubectl apply -f pv_mysql.yaml
 kubectl apply -f pv_wordpress.yaml
 kubectl apply -f pvc_sql.yaml
 kubectl apply -f pvc_wordpress.yaml
+# Apply Deployment and Service
 kubectl apply -f deployment_sql.yaml
 kubectl apply -f deployment_wordpress.yaml
-kubectl apply -f mysql_service.yaml
-kubectl apply -f wordpress_service.yaml
+kubectl apply -f service_mysql.yaml
+kubectl apply -f service_wordpress.yaml
 kubectl apply -f passsql.yaml
